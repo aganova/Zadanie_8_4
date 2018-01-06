6a7cd11de73c4e18f844f1c6e27233fd6a23074f
Agnieszka Nowacka <aguo84@gmail.com>
2018-01-06 17:55:44 +0100

    Update file scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
+/* exercise - practicing comments
+e.g. console.log('The commented will not be called'); */
+
+var name = prompt('Enter your name'); //variable that holds name
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
 
